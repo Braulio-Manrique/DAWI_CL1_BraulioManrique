4b6e388340a5305f9273709100ca3ef8aca3637f
Braulio Manrique <brauliocuenta12@gmail.com>
2024-04-29 08:30:47 -0500

    Iniciando Examen Braulio

---
+++ DAWI_CL1_Braulio_Manrique/src/main/java/pe/Cibertec/DAWI_CL1_Braulio_Manrique/DawiCl1BraulioManriqueApplication.java
@@ -0,0 +1,13 @@
+package pe.Cibertec.DAWI_CL1_Braulio_Manrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1BraulioManriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1BraulioManriqueApplication.class, args);
+	}
+
+}
---
+++ DAWI_CL1_Braulio_Manrique/src/test/java/pe/Cibertec/DAWI_CL1_Braulio_Manrique/DawiCl1BraulioManriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.Cibertec.DAWI_CL1_Braulio_Manrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1BraulioManriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
